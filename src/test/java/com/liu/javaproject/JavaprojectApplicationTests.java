93fba069ba3f8dbfab0005133fc5a970944504eb
liuzongxin <360627959@qq.com>
2020-07-22 10:55:02 +0800

    初始化项目
    Signed-off-by: liuzongxin <360627959@qq.com>

--- src/test/java/com/liu/javaproject/JavaprojectApplicationTests.java
+++ src/test/java/com/liu/javaproject/JavaprojectApplicationTests.java
@@ -1,13 +1,22 @@
 package com.liu.javaproject;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavaprojectApplicationTests {
 
+    @Autowired
+    StringEncryptor stringEncryptor;
+
     @Test
-    void contextLoads() {
+    public void encrytPwd(){
+        String result = stringEncryptor.encrypt("mySecretKey");
+        System.out.println("==================");
+        System.out.println(result);
+        System.out.println("==================");
     }
 
 }
